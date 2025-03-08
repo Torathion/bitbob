27d4415679018863d6e7eb085f18f1d897ed4604
Torathion <torathion@gmail.com>
2025-03-08 21:55:10 +0100

    dev: lint

--- src/utils/math/round.ts
+++ src/utils/math/round.ts
@@ -6,5 +6,5 @@
  */
 export default function round(x: number): number {
   const frac = x - (x | 0) + 0
-  return (x + frac + (frac !== 0 ? 0.1 : 0)) | 0
+  return (x + frac + (frac ? 0.1 : 0)) | 0
 }
